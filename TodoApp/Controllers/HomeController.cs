d61c202d42c240d291817f3c732ee6f565317ad5
diaz-L <lediar.a.diaz@gmail.com>
2021-11-13 18:33:39 -0500

    modified Home controller to list all todo items

--- TodoApp/Controllers/HomeController.cs
+++ TodoApp/Controllers/HomeController.cs
@@ -4,7 +4,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using TodoApp.Entities;
 using TodoApp.Models;
 
 namespace TodoApp.Controllers
@@ -12,15 +14,24 @@ namespace TodoApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly TodoContext _ctx;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, TodoContext ctx)
         {
             _logger = logger;
+            _ctx = ctx;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var todos = _ctx.Todos.ToList();
+
+            if (todos is null)
+            {
+                return NoContent();
+            }
+            
+            return View(todos);
         }
     }
 }
