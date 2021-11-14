bf51a90d7330fb8bcbae964a145a87cdc12f81e4
diaz-L <lediar.a.diaz@gmail.com>
2021-11-13 19:53:25 -0500

    -modified Index view to include link to Edit 'Todo' -added Edit action methods to TodoController to perform edits on entity

--- TodoApp/Controllers/TodoController.cs
+++ TodoApp/Controllers/TodoController.cs
@@ -31,5 +31,34 @@ namespace TodoApp.Controllers
             
             return View(todo);
         }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var todo = _ctx.Todos.Find(id);
+
+            if (todo is null)
+            {
+                ViewBag.NotFound = true;
+                return RedirectToAction("Index", "Home");
+            }
+            
+            return View(todo);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(TodoItem todo)
+        {
+            if (ModelState.IsValid)
+            {
+                _ctx.Todos.Update(todo);
+                _ctx.SaveChanges();
+                
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(todo);
+        }
+        
     }
 }
